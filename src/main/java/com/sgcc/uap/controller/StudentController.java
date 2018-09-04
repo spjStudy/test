34f3f9481eb48d5b289da4c7cf44858ea0554c9e
spj <870419617@qq.com>
2018-09-04 16:31:35 +0800

    1
    Change-Id: I3cfcf992582913232f3caab7dd14615ff6489e86
    Signed-off-by: spj <870419617@qq.com>

--- src/main/java/com/sgcc/uap/controller/StudentController.java
+++ src/main/java/com/sgcc/uap/controller/StudentController.java
@@ -59,7 +59,7 @@ public class StudentController {
 	}
 	@RequestMapping(value = "/student/{sno}")
 	public List<Object> getByStudent(@PathVariable String sno) {
-		System.out.println("我的心好累啊一个分支");
+		System.out.println("我的心好累啊一个分支112312313");
 		return studentRepository.getByUserId();
 	}
 }
